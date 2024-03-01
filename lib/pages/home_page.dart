53f80aa627d260acc657808a53db73a467f1978b
Jinesh013 <jineshparekh013@gmail.com>
2024-03-01 17:05:34 +0530

    commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,17 +5,19 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Catalog App"),
-      ),
-      body: Center(
-        // ignore: avoid_unnecessary_containers
-        child: Container(
-          child: const Text("THis is Home Page"),
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Catalog App"),
         ),
+        body: Center(
+          // ignore: avoid_unnecessary_containers
+          child: Container(
+            child: const Text("THis is Home Page"),
+          ),
+        ),
+        drawer: const Drawer(),
       ),
-      drawer: const Drawer(),
     );
   }
 }
