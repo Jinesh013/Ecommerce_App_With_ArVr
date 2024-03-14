51e3367e063b70a49b48e597868643fb3cfbab30
Jinesh013 <jineshparekh013@gmail.com>
2024-03-14 13:46:18 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:ecomapp/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
